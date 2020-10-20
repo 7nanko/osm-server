bfd705b9b967a0b97cfbac567fba2e125d5d6fd3
ak <akaiwa@jrce.co.jp>
2020-10-20 12:51:20 +0900

    add instructions.

--- render.sh
+++ render.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
-# https://hub.docker.com/r/overv/openstreetmap-tile-server
+# Download japan-latest.osm.pbf from https://download.geofabrik.de/asia/japan.html
+# and follow https://hub.docker.com/r/overv/openstreetmap-tile-server
 
 # docker volume create osm-data
 
@@ -9,6 +10,8 @@
 #     -v osm-data:/var/lib/postgresql/12/main \
 #     overv/openstreetmap-tile-server \
 #     import
+
+# docker build -t osm-server .
 
 docker run \
     -v $PWD/tiles:/tiles \
